c1c77cd5dfae21c9353fa8a4fa0f6795f4f0ecad
Mark B <mbecerra@sparefoot.com>
2015-10-19 10:34:01 -0500

    make it work, for real

--- tasks/swig_precompile_amd.js
+++ tasks/swig_precompile_amd.js
@@ -1,6 +1,6 @@
 /*
  * grunt-swig-precompile-amd
- * https://github.com/mbecerra/grunt-swig-precompile-tojs
+ * https://github.com/markula/grunt-swig-precompile-amd
  *
  * Copyright (c) 2015 Mark B
  * Licensed under the MIT license.
@@ -8,122 +8,42 @@
 
 'use strict';
 
+var swig = require("swig"),
+    path = require("path"),
+    fs = require('fs'),
+    beautify_html = require("js-beautify").html;
+
+
 module.exports = function(grunt) {
 
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('swig_precompile_amd', 'Precompile a swig or twig template to an amd module.', function() {
-  //   // Merge task-specific and/or target-specific options with these defaults.
-  //   var options = this.options({
-  //     cache: false,
-  //     force: true
-  //   });
-
-  //   // Iterate over all specified file groups.
-  //   this.files.forEach(function(f) {
-  //     // Concat specified files.
-  //     var src = f.src.filter(function(filepath) {
-  //       // Warn on and remove invalid source files (if nonull was set).
-  //       if (!grunt.file.exists(filepath)) {
-  //         grunt.log.warn('Source file "' + filepath + '" not found.');
-  //         return false;
-  //       } else {
-  //         return true;
-  //       }
-  //     }).map(function(filepath) {
-  //       // Read file source.
-  //       return grunt.file.read(filepath);
-  //     }).join(grunt.util.normalizelf(options.separator));
-
-  //     // Handle options.
-  //     src += options.punctuation;
-
-  //     // Write the destination file.
-  //     grunt.file.write(f.dest, src);
-
-  //     // Print a success message.
-  //     grunt.log.writeln('File "' + f.dest + '" created.');
-  //   });
-  // });
-
-// get use config option, default options will be overwritten
+  grunt.registerMultiTask('swig_precompile_amd', 'Grunt plugin that will convert Twig/Swig templates to executable functions in amd modules.', function() {
+    // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      cache: false,
-      force: true // compile all files
+      punctuation: '.',
+      separator: ', '
     });
 
-    options.beautify = options.beautify || {};
-
-    //swig.setDefaults(options);
-
-    var now = Date.now(),
-        changeFiles = [],
-        files = [],
-        compileAll = false;
-
+    // Iterate over all specified file groups.
     this.files.forEach(function(f) {
-      var filepath = f.src[0],
-          filename = path.basename(filepath),
-          isPartial = (/^_/).test(filename),
-          stat;
 
-      if(!grunt.file.exists(filepath)) {
-        grunt.log.warn('Source file "' + filepath + '" not found.');
-        return;
-      }
+      var src = f.src;
+      var dest = f.dest.replace('.twig', '.js');
+      var contents = grunt.file.read(f.src);
+      var _tpl = swig.precompile(contents, {}).tpl.toString().replace('anonymous', '');
 
-      // store file which is not start with '_'
-      if(!isPartial) {
-        files.push(f);
-      }
+      _tpl = 'define(function () { return ' + _tpl + '; });';
 
-      // check file is change
-      stat = fs.statSync(path.resolve(filepath));
-      if(now - stat.mtime.getTime() < 5000 || options.force) {
-        if(isPartial) {
-          // the changed file is start with '_', and will compile all files
-          compileAll = true;
-          return;
-        }
-        changeFiles.push(f);
-      }
-
-    });
-    
-
-    changeFiles = compileAll ? files : changeFiles;
-
-    changeFiles.forEach(function (f) {
-      var url = f.src[0],
-          splitUrl = url.split('/'),
-          length = splitUrl.length;
-
-      var tmp = splitUrl[length-1].split('.')[0];
-
-      var Toptions = options;
-      Toptions.locals.active = tmp;
-      //console.log(Toptions)
-      swig.setDefaults(Toptions);
-
-      var dest = f.dest.substring(0, f.dest.lastIndexOf('.')) + '.html';
-      var src = path.resolve(f.src[0]);
-
-
-      var r = swig.precompile(str, { filename: file, locals: ctx }).tpl.toString().replace('anonymous', '');
-
-      console.dir(r);
+      // Handle options.
+      // src += options.punctuation;
 
       // Write the destination file.
-      grunt.file.write(
-        dest,
-        beautify_html(
-          swig.renderFile(src, {}),
-          options.beautify
-        )
-      );
+      grunt.file.write(dest, _tpl);
 
       // Print a success message.
-      grunt.log.writeln('write file: ' + dest);
-
+      grunt.log.writeln('File "' + dest + '" created.');
+    });
+  });
 };
